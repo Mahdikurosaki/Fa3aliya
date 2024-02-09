240bf56f977644452c6edd46a9b34805576a87e1
Mahdikurosaki <154265304+Mahdikurosaki@users.noreply.github.com>
2024-02-09 06:53:25 +0000

    Update johan.js

--- johan.js
+++ johan.js
@@ -1,5 +1,3 @@
-console.log('Ejecuntando al bot mas shidori');
-
 import { join, dirname } from 'path'
 import { createRequire } from 'module'
 import { fileURLToPath } from 'url'
@@ -7,39 +5,39 @@ import { setupMaster, fork } from 'cluster'
 import { watchFile, unwatchFile } from 'fs'
 import cfonts from 'cfonts';
 import { createInterface } from 'readline'
-import Helper from './lib/helper.js'
 import chalk from 'chalk'
 
 // https://stackoverflow.com/a/50052194
 const __dirname = dirname(fileURLToPath(import.meta.url))
-const require = createRequire(__dirname) // Bring in the ability to create the 'require' method
+const require = createRequire(import.meta.url) // Bring in the ability to create the 'require' method
 const { name, author } = require(join(__dirname, './package.json')) // https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
 say(`JOHAN-BOT-MD`, {
   font: 'pallet',
-    color: 'candy',
-    align: 'center',
-    gradient: ["red","blue"]
+  color: 'candy',
+  align: 'center',
+  gradient: ["red","blue"]
 })
 
 say((`BOT 2024`), {
-    font: 'simple3d',
-    color: 'candy',
-    align: 'center',
-    gradient: ["red","magenta"],
-    lineHeight: 3
-  })
+  font: 'simple3d',
+  color: 'candy',
+  align: 'center',
+  gradient: ["red","magenta"],
+  lineHeight: 3
+})
 
 var isRunning = false
-/**Start a js file
-@param {String} file `path/to/file`*/
+
+/** Start a js file
+* @param {String} file `path/to/file`
+*/
 function start(file) {
   if (isRunning) return
   isRunning = true
   let args = [join(__dirname, file), ...process.argv.slice(2)]
-  ///say([process.argv[0],...args].join(' '),{font:'console',align:'center',gradient:['red','magenta']})
   setupMaster({
     exec: args[0],
     args: args.slice(1),
@@ -60,7 +58,7 @@ function start(file) {
   })
   p.on('exit', (_, code) => {
     isRunning = false
-    console.error(chalk.bgRed('\n\n[!] Salió del código : '), chalk.bgWhite(code+'\n'))
+    console.log(chalk.bgRed('\n\n[!] Salió del código : '), chalk.bgWhite(code+'\n'))
     p.process.kill() 
     isRunning = false
     start.apply(this, arguments)
@@ -70,11 +68,13 @@ function start(file) {
       start(file)
     })
   })
-  if (!Helper.opts['test'])
-    if (!rl.listenerCount()) rl.on('line', line => {
-      p.emit('message', line.trim())
-    })
-  // console.log(p)
+  if (!Helper.opts['test']) {
+    if (!rl.listenerCount('line')) {
+      rl.on('line', line => {
+        p.emit('message', line.trim())
+      })
+    }
+  }
 }
 
 start('main.js')
